436a044e5fe3fec405e1a7e0758d93317cd61dcf
khanhnn <nguyenkhanhh.0205@gmail.com>
2024-08-17 00:03:39 +0700

    using axios lib

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -9,6 +9,7 @@ import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
 import styles from './Search.module.scss';
 import { useDebounce } from '~/components/hooks';
+import * as searchServices from "../../apiSevices/searchService";
 
 
 
@@ -29,17 +30,17 @@ const Search = () => {
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
+
     }, [debounced]);
 
     const handleClear = () => {
