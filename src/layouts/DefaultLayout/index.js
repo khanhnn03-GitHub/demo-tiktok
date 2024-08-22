3bf3c2b630cf8b7f10371b06ccab98eafece9322
khanhnn <nguyenkhanhh.0205@gmail.com>
2024-08-23 01:16:29 +0700

    build UI sidebar part 1

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,9 +1,10 @@
 import classNames from "classnames/bind";
 import Header from "~/layouts/components/Header";
 import styles from "./DefaultLayout.module.scss";
-import Sidebar from "./Sidebar";
 import React from 'react';
 import PropTypes from "prop-types";
+import Sidebar from "../../components/Sidebar";
+
 const cx = classNames.bind(styles);
 
 const DefaultLayout = ({children}) => {
